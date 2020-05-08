b108698a3104eb0d24d2e9c8bb6c48bcd9c0bfec
Rafid <rafida9@gmail.com>
2020-05-08 13:30:37 -0400

    log in page sucessfuly quering the data base and loging in to return user id

--- Login_page.py
+++ Login_page.py
@@ -1,6 +1,7 @@
 import psycopg2
 from psycopg2 import OperationalError
 
+
 #connecting to the postgreSQL localserver
 def create_connection(db_name, db_user, db_password, db_host, db_port):
     connection = None
@@ -34,7 +35,7 @@ def login_verification():
 
     while loggedin == False:
 
-        useremail_input = input('user email?')
+        useremail_input =input('user email?') 
         useremail= ("'"+ useremail_input + "'")
         user_password = input('password?')
         userpassword= ("'"+ user_password + "'")
@@ -42,17 +43,18 @@ def login_verification():
         #because of the "@" in the email PostgreSQL the query doesnt work so the email is entered as a string thus adding extra quotation marks 
         query = f'SELECT count(*) FROM registration WHERE email = {useremail} and password ={userpassword}'
 
-        details_query = f'SELECT (id,name) FROM registration WHERE email = {useremail} and password ={userpassword} '
+        details_query= f'SELECT id,name FROM registration WHERE email = {useremail} and password ={userpassword} '
+
 
         return_text=execute_read_query(connection, query)
 
         if return_text[0][0]>0:
-            print('Log in successful')
             user_detail=execute_read_query(connection, details_query)
-            print(user_detail[0][0][1] )
-            
-            loggedin = True
-            return user_detail
+            user_id = user_detail[0][0]
+            name =user_detail[0][1]
+            list_return=[user_id,name]
+            print(f'Hi! {name}, you are logged in')
+            return list_return
         else:
             print('Please type correct Email and Password or Sign up')
 
